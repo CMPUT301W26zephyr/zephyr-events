827b4358ae532786f2dd0fce8ee2d10072b935cf
Yani <Yanixiv.dev@gmail.com>
2026-02-18 20:47:35 -0700

    WIP: scaffold project structure and start model layer

---
+++ code/app/src/main/java/com/example/zephyrevents/model/Event.java
@@ -0,0 +1,14 @@
+package com.example.zephyrevents.model;
+
+import java.time.LocalDateTime;
+
+/**
+ * This is a class that defines an event.
+ */
+public class Event {
+    private  String id;
+    private  String name;
+    private  LocalDateTime startTime;
+    private  LocalDateTime endTime;
+
+}
---
+++ code/app/src/main/java/com/example/zephyrevents/model/User.java
@@ -0,0 +1,13 @@
+package com.example.zephyrevents.model;
+
+/**
+ * This is a class that defines a user.
+ */
+public class User {
+    private String deviceId;
+    private String name;
+    private String email;
+    private String phone;
+    private boolean notificationsOptOut; // Opt out of all notifications account wide?
+    private String location;
+}
