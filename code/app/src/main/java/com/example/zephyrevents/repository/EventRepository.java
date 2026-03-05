9e2592f9d652a626cfe1c3bb47e2d7efcbbec25b
Yani <Yanixiv.dev@gmail.com>
2026-03-05 01:49:23 -0700

    WIP: Event Repo

--- code/app/src/main/java/com/example/zephyrevents/repository/EventRepository.java
+++ code/app/src/main/java/com/example/zephyrevents/repository/EventRepository.java
@@ -1,7 +1,15 @@
 package com.example.zephyrevents.repository;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
 import com.example.zephyrevents.model.Event;
 import java.util.List;
+import java.util.Objects;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 
@@ -9,13 +17,43 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class EventRepository {
 
     private final FirebaseFirestore db;
+    private static final String TAG = "EventRepository";
 
     public EventRepository() {
         db = FirebaseFirestore.getInstance();
     }
 
     public void createEvent(Event event, RepositoryCallback<Void> callback) {
-        //TODO: FIREBASE CODE
+        if (event == null) {
+            var e = new IllegalArgumentException("Event cannot be null");
+            Log.w(TAG, "event passed had value of null", e);
+            callback.onFailure(e);
+            return;
+        }
+        if (event.getId() == null || event.getId().trim().isEmpty()){
+            var e = new IllegalArgumentException("Event id passed has no value");
+            Log.w(TAG, "event has no associated id", e);
+            callback.onFailure(e);
+            return; // exit before network call.
+        }
+        db.collection(Collections.EVENTS)
+                .document(event.getId())
+                .set(event)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "Firestore event added object id: " + event.getId());
+                        callback.onSuccess(null);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                        callback.onFailure(e);
+                    }
+                });
+
     }
 
     public void getEventById(String id, RepositoryCallback<Event> callback) {
