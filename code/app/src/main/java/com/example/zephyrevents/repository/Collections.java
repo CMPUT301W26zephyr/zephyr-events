94fccb324c97bc4b9a5cd5e9e5f533827980efac
Yani <Yanixiv.dev@gmail.com>
2026-03-06 06:33:50 -0700

    WIP: UserRespository

--- code/app/src/main/java/com/example/zephyrevents/repository/Collections.java
+++ code/app/src/main/java/com/example/zephyrevents/repository/Collections.java
@@ -5,4 +5,5 @@ public final class Collections {
     // constants for firebase collections, avoids magic strings everywhere, prevents bugs.
     public static final String EVENTS = "events";
 
+    public static final String USERS = "users";
 }
--- code/app/src/main/java/com/example/zephyrevents/repository/UserRepository.java
+++ code/app/src/main/java/com/example/zephyrevents/repository/UserRepository.java
@@ -1,6 +1,14 @@
 package com.example.zephyrevents.repository;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.example.zephyrevents.model.Event;
 import com.example.zephyrevents.model.User;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 //consider using collection.
@@ -12,12 +20,75 @@ public class UserRepository {
         db = FirebaseFirestore.getInstance();
     }
 
+    private static final String TAG = "UserRepository";
     public void createUser(User user, RepositoryCallback<Void> callback) {
-        // TODO: FIREBASE CODE
+        saveUser(user, callback);
+    }
+
+    public void saveUser(User user, RepositoryCallback<Void> callback) {
+        if (user == null) {
+            var e = new IllegalArgumentException("User cannot be null");
+            Log.w(TAG, "User passed had value of null", e);
+            callback.onFailure(e);
+            return;
+        }
+        if (user.getId() == null || user.getId().trim().isEmpty()){
+            var e = new IllegalArgumentException("user id passed has no value");
+            Log.w(TAG, "user has no associated id", e);
+            callback.onFailure(e);
+            return; // exit before network call.
+        }
+        db.collection(Collections.USERS)
+                .document(user.getId())
+                .set(user)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "firestore event added object id: " + user.getId());
+                        callback.onSuccess(null);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                        callback.onFailure(e);
+                    }
+                });
+
     }
 
     public void getUserById(String id, RepositoryCallback<User> callback) {
-        // TODO: FIREBASE CODE
+        if (id == null || id.trim().isEmpty()){
+            var e = new IllegalArgumentException("user id passed has no value");
+            Log.w(TAG, "invalid user id", e);
+            callback.onFailure(e);
+            return; // exit before network call.
+        }
+        db.collection(Collections.USERS)
+                .document(id)
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot doc) {
+                        if (doc == null || !doc.exists()){
+                            var e = new IllegalArgumentException("document returned doesn't exist");
+                            Log.w(TAG, "user document not found for id: "+id, e);
+                            callback.onFailure(e);
+                        }else{
+                            Log.d(TAG, "DocumentSnapshot data: " + doc.getData());
+                            callback.onSuccess(doc.toObject(User.class));
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "error getting doc by id: "+id+"\n exception returned: ", e);
+                        callback.onFailure(e);
+                    }
+                });
+
     }
 
     public void updateUser(User user, RepositoryCallback<Void> callback) {
@@ -25,8 +96,29 @@ public class UserRepository {
     }
 
     public void deleteUser(String id, RepositoryCallback<Void> callback) {
-        // TODO: FIREBASE CODE
-    }
+        if (id == null || id.trim().isEmpty()){
+            var e = new IllegalArgumentException("user id passed has no value");
+            Log.w(TAG, "invalid user id", e);
+            callback.onFailure(e);
+            return; // exit before network call.
+        }
+        db.collection(Collections.USERS)
+                .document(id)
+                .delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "user deleted successfully id: " + id);
+                        callback.onSuccess(null);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "error deleting user with id: "+id+"\n exception returned: ", e);
+                        callback.onFailure(e);
+                    }
+                });    }
 
     public void updateNotificationOptOut(String userId,
                                          boolean optOut,
