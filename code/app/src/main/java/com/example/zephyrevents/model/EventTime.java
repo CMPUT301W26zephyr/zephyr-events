4982036fa5d648845519a2d03a63c4a145b1b5b7
Yani <Yanixiv.dev@gmail.com>
2026-03-08 05:57:50 -0600

    refactor: pulled eventTime into its own class

--- code/app/src/main/java/com/example/zephyrevents/model/EventTime.java
+++ code/app/src/main/java/com/example/zephyrevents/model/EventTime.java
@@ -1,12 +1,27 @@
 package com.example.zephyrevents.model;
 
+import com.example.zephyrevents.util.DistanceHelper;
+import com.example.zephyrevents.util.TimeHelper;
+
 public class EventTime {
     private  long startTime;
     private  long endTime;
     public EventTime(){}
 
+
+
     public EventTime(long startTime, long endTime){
         this.startTime= startTime;
         this.endTime = endTime;
+    }
+
+    /**
+     * Makes it super easy to get a formatted string of how much time is left for an event.
+     * @return formatted string that shows how much time is left for an event.
+     */
+    public String eventTimeLeft(){
+        var timeNow = TimeHelper.now();
+        var timeLeft = TimeHelper.timeDifference(endTime, timeNow);
+        return TimeHelper.formatDuration(TimeHelper.timeDifference(timeNow, timeLeft));
     }
 }
