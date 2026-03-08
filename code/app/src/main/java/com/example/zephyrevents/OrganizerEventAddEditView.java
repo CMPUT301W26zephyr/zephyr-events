ce8721747112185abfa60bb3ec11c1f437d21361
“Saad-Mohiuddin” <msmohiud@ualberta.ca>
2026-03-07 23:47:11 -0700

    Creation screen 1

--- code/app/src/main/java/com/example/zephyrevents/OrganizerEventAddEditView.java
+++ code/app/src/main/java/com/example/zephyrevents/OrganizerEventAddEditView.java
@@ -1,9 +1,13 @@
 package com.example.zephyrevents;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class OrganizerEventAddEditView extends AppCompatActivity {
 
@@ -12,5 +16,14 @@ public class OrganizerEventAddEditView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.event_creation1);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.bottom_button_container), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(0, 0, 0, systemBars.bottom);
+            return insets;
+        });
+
+        Button nextButton = findViewById(R.id.next_button);
+        nextButton.setText("Next");
     }
 }
