585b17fd2c3e7a6f4f94bcdc8c8ff6f2790f4b48
Yani <Yanixiv.dev@gmail.com>
2026-03-12 21:25:26 -0600

    add: EventRepositoryTest

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -33,6 +33,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    testOptions {
+        unitTests {
+            isReturnDefaultValues = true
+        }
+    }
 }
 
 dependencies {
@@ -46,6 +51,7 @@ dependencies {
 
     testImplementation("org.mockito:mockito-core:5.23.0")  // Mockito framework
     testImplementation("org.mockito:mockito-inline:5.2.0")  // Allows mocking final classes
+
 
 
     // Import the Firebase BoM
